7616a8e7ea555626ae697ca26e4e701929b8ecf3
Tarun-captain <80529091+Tarun-captain@users.noreply.github.com>
2022-02-07 20:42:55 +0530

    Add files via upload

---
+++ Create DATABASE # ;.sql
@@ -0,0 +1,14 @@
+Create DATABASE # ;
+
+CREATE TABLE Student(
+
+FName VARCHAR(50), 
+LastName VARCHAR(30),
+ROllnumber VARCHAR(1000),
+address VARCHAR(60) ,
+city VARCHAR(25) ,
+Gmail int not null, 
+PRIMARY key (gmail)
+)
+
+SELECT * from Student 
